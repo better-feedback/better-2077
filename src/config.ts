2f3eb75f0ccf349279e990c40deee19d94d9a849
Chris <25006584+seichris@users.noreply.github.com>
2024-07-15 18:12:40 +0400

    Update config.ts with 2077 title and links

--- src/config.ts
+++ src/config.ts
@@ -23,13 +23,13 @@ export const nearChainConfig = {
 
 export const siteConfig = {
   enabledChains: ["near" , "polygon"],
-  projectName: "Better",
-  title: "Better Roadmap",
-  metaDescription: "Better Roadmap",
-  url: "https://roadmap.betterhq.org/",
+  projectName: "2077 Bounties",
+  title: "2077 Bounties",
+  metaDescription: "2077 Bounties",
+  url: "https://ideas.8o.vc/",
   metaImg: "https://raw.githubusercontent.com/better-feedback/better-app/1926cf0a2327e629128f65e57edeee7440294e0b/public/metaTagImg.jpg?token=AF6ZD6A7XI4IS7I3MRLDQSDCTY6QK",
   externalLinks: {
-    docs: "https://github.com/better-feedback/roadmap#readme",
+    docs: "https://github.com/better-feedback/better-2077#readme",
     discord: "wwwwRFa6aj",
     twitter: "betterdao",
   },
